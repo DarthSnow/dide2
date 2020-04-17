50f103ae174a04823b7ea464e5ba750b8b47e0d6
Basile Burg <basile.b@gmx.com>
2020-04-17 13:46:23 +0200

    fix linux zip files

--- setup/zip-nux32.sh
+++ setup/zip-nux32.sh
@@ -6,7 +6,7 @@ cp * $fld/
 zip -9 \
 ../output/dexed.${ver:1:100}.linux32.zip \
 $fld/dcd.license.txt $fld/dexed.license.txt \
-$fld/dexed \
+$fld/dexed libdexed-d.so \
 $fld/dexed.ico $fld/dexed.png \
 $fld/dcd-server $fld/dcd-client $fld/dscanner
 rm -rf dexed-x86
