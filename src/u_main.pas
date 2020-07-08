DarthSnow/dide2

commit 87218256b689ceb74fea9e302e87cbf2fd83daf3

fix #53 - project, git branches, last commit message in a tooltip

src/u_main.pas
@@ -2803,6 +2803,17 @@ begin
   end;
 end;
 
+{procedure TMainForm.ActionsExecute(AAction: TBasicAction; var Handled: Boolean);
+var
+  HintAction: TCustomHintAction absolute AAction;
+begin
+  if (AAction is TCustomHintAction) and
+    string(HintAction.Hint).StartsWith(<magic string>) then
+  begin
+    // here attempt to show TMenuItem.Hint can be detected
+  end;
+end;}
+
 procedure TMainForm.actLayoutResetExecute(Sender: TObject);
 begin
   InitDocking(true);
@@ -4162,6 +4173,7 @@ procedure TMainForm.actProjGitBranchesUpdExecute(Sender: TObject);
 var
   p: TProcess;
   r: TStringList;
+  c: TStringList;
   i: integer;
   m: TMenuItem;
   a: boolean;
@@ -4176,6 +4188,7 @@ begin
     exit;
   p := TProcess.Create(nil);
   r := TStringList.Create;
+  c := TStringList.create;
   try
     p.Executable := exeFullName('git' + exeExt);
     if p.Executable.fileExists then
@@ -4195,6 +4208,11 @@ begin
         mnuGitBranch.Add(m);
         mnuGitBranch.AddSeparator;
       end;
+      c.LoadFromStream(p.Stderr);
+      for i := 0 to c.Count-1 do
+        fMsgs.message(c[i], fProj, amcProj, amkAuto);
+      p.Parameters.Clear();
+      p.Parameters.AddStrings(['log', '--format=%B', '-n', '1', '<branch...>']);
       for i:= 0 to r.Count-1 do
       begin
         m := TMenuItem.Create(mnuGitBranch);
@@ -4208,16 +4226,21 @@ begin
           m.Checked:= true;
         end
         else m.Caption:= Trim(r[i][1..r[i].length]);
+        c.Clear;
+        p.Parameters.Delete(4);
+        p.Parameters.AddStrings(m.Caption);
+        p.Execute();
+        processOutputToStrings(p, c);
+        while p.Running do ;
+        if not c.Count.equals(0) then
+          m.caption := m.caption + '  [last commit: ' + c[0] + ' ]';
         mnuGitBranch.Add(m);
       end;
-      r.Clear;
-      r.LoadFromStream(p.Stderr);
-      for i := 0 to r.Count-1 do
-        fMsgs.message(r[i], fProj, amcProj, amkAuto);
     end;
   finally
     p.Free;
     r.Free;
+    c.Free;
   end;
 end;
 

src/u_main.lfm
@@ -11,7 +11,6 @@ object MainForm: TMainForm
   OnDropFiles = FormDropFiles
   OnResize = FormResize
   ShowHint = True
-  LCLVersion = '2.0.8.0'
   object mainMenu: TMainMenu
     top = 1
     object MenuItem1: TMenuItem

commit 1001642f88e4b2d16a7a8a9d2706abdaad5216f5

fix, checkout from menu item caption broken
caused by 87218256b689ceb74fea9e302e87cbf2fd83daf3

src/u_main.pas
@@ -4146,7 +4146,7 @@ begin
     exit;
   p := TProcess.Create(nil);
   r := TStringList.Create;
-  b := TMenuItem(sender).Caption;
+  b := TMenuItem(sender).Hint;
   try
     p.Executable := exeFullName('git' + exeExt);
     if p.Executable.fileExists then
@@ -4222,18 +4222,18 @@ begin
         m.OnClick:= @gitBranchMenuItemClick;
         if r[i][1] = '*' then
         begin
-          m.Caption:= Trim(r[i][2..r[i].length]);
+          m.Hint:= Trim(r[i][2..r[i].length]);
           m.Checked:= true;
         end
-        else m.Caption:= Trim(r[i][1..r[i].length]);
+        else m.Hint:= Trim(r[i][1..r[i].length]);
         c.Clear;
         p.Parameters.Delete(4);
-        p.Parameters.AddStrings(m.Caption);
+        p.Parameters.AddStrings(m.Hint);
         p.Execute();
         processOutputToStrings(p, c);
         while p.Running do ;
         if not c.Count.equals(0) then
-          m.caption := m.caption + '  [last commit: ' + c[0] + ' ]';
+          m.caption := m.Hint + '  [last commit: ' + c[0] + ' ]';
         mnuGitBranch.Add(m);
       end;
     end;